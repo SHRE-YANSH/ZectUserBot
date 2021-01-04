4f6b91951820ad16706c158d06fef91fafa68614
okay-retard <68938378+okay-retard@users.noreply.github.com>
2021-01-04 18:44:46 +0530

    Update pmpermitdb.py

--- Zect/database/pmpermitdb.py
+++ Zect/database/pmpermitdb.py
@@ -45,7 +45,7 @@ def get_pm_settings():
     pm_message = result.get("pmpermit_message", PMPERMIT_MESSAGE)
     block_message = result.get("block_message", BLOCKED)
     limit = result.get('limit', LIMIT)
-    return pmpermit, pm_message, limit
+    return pmpermit, pm_message, limit, block_message
 
 
 def allow_deny(chat, value):
