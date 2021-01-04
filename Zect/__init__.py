5cba8ad60689837545632c0b77f1c4630fcd5c3b
okay-retard <itnakim161@gmail.com>
2021-01-05 00:41:13 +0530

    enable logging

--- Zect/__init__.py
+++ Zect/__init__.py
@@ -8,7 +8,9 @@ import logging
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+LOGGER = logging.getLogger(__name__)
 
 HELP = {}
 CMD_HELP = {}
--- Zect/__main__.py
+++ Zect/__main__.py
@@ -1,6 +1,7 @@
 from pyrogram import idle, Client
-from Zect import app
+from Zect import app, LOGGER
 from Zect.modules import *
 
 app.start()
+LOGGER.info("Your Zect UserBot is ready for use type .help in any telegram chat.")
 idle()
