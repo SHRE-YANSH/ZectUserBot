b690c7208c61c22f0ec0760f08004b862b2c47f1
Shreyansh <68938378+okay-retard@users.noreply.github.com>
2021-03-04 15:09:53 +0530

    Update help.py

--- Zect/modules/help.py
+++ Zect/modules/help.py
@@ -9,7 +9,7 @@ HELP.update(
         "**AFK**": "__afk, unafk__",
         "**Alive**": "__alive, ping__",
         "**Developer**": "__eval, term__",
-        "**Misc**": "__paste, tr, whois, id__, purge, del",
+        "**Misc**": "__paste, tr, whois, id__",
         "**Notes**": "__save, get, clear, clearall, notes__",
         "**Anti-PM**": "__pmguard, setpmmsg, setlimit, setblockmsg, allow, deny__",
         "**Sticker**": "__kang, stkrinfo__",
