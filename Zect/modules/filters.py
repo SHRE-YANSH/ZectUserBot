54338cda12e72e80876f86a42d75f602f82d27d8
Shreyansh <68938378+okay-retard@users.noreply.github.com>
2021-04-19 10:24:43 -0700

    Update filters.py

--- Zect/modules/filters.py
+++ Zect/modules/filters.py
@@ -73,7 +73,7 @@ async def s_filters(client, message):
     await note_.edit(f"**Done! `{note_name}` Added To Filters List!**")
 
 
-@app.on_message(filters.incoming & ~filters.edited & filters.group, group=-2)
+@app.on_message(filters.incoming & ~filters.edited & filters.group, group=0)
 async def filter_s(client, message):
     owo = message.text
     al_fill = []
